34c4954cdea895895418b1e5b85235552f84d4c8
Vignesh100799 <baluvignesh422@gmail.com>
2023-12-26 11:55:36 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import CreateNews from './components/CreateNews';
+import EditNews from './components/EditNews';
+import CreateCategory from './components/CreateCategory';
+import EditCategory from './components/EditCategory';
+import ListAllNews from './components/ListAllNews';
+import ListAllCategory from './components/ListAllCategory';
 function App() {
   return (
-   <>
-   <h1>Admin App</h1></>
+    <>
+      <BrowserRouter>
+     <Routes>
+    <Route path='/' element={<Login/>}></Route>
+    <Route path='/create-news' element={<CreateNews/>}></Route>
+    <Route path='/edit-news' element={<EditNews/>}></Route>
+    <Route path='/list-all-news' element={<ListAllNews/>}></Route>
+    <Route path='/create-category' element={<CreateCategory/>}></Route>
+    <Route path='/edit-category' element={<EditCategory/>}></Route>
+    <Route path='/list-all-category' element={<ListAllCategory/>}></Route>
+     </Routes>
+      
+      </BrowserRouter>
+      </>
   );
 }
 
---
+++ src/components/ListAllNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListAllNews = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListAllNews
