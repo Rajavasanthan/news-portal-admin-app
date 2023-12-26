e9c9e19a0c5b430bbd0feb50cf57c91986c16818
Arasamannar A K <arasamannar@gmail.com>
2023-12-26 20:50:27 +0530

    using formik created a login page which requests username and password for login as Admin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import { useFormik } from "formik";
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validate: (values) => {
+      const errors = {};
+      if (!values.username.trim()) {
+        errors.username = "Please enter the Username";
+      } else if (values.username.length < 3 || values.username.length > 12) {
+        errors.username = "Please enter a value between 3 and 12 characters";
+      }
 
-export default Login
+      if (!values.password.trim()) {
+        errors.password = "Please enter the Password";
+      }
+      console.log(errors)
+      return errors;
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100 bg-info bg-gradient">
+      <div className="container bg-info bg-light mt-5 shadow p-3 mb-5 bg-body rounded">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <h3 className="text-center"><u>Admin Login</u></h3>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="username">
+                  {" "}
+                  Username:
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="form-control "
+                  autoComplete="true"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <span className="text-danger">{formik.errors.username}</span>
+              </div>
+              <br />
+              <div className="mb-3">
+                <label className="form-label" htmlFor="pass">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  id="pass"
+                  name="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  className="form-control"
+                />
+                <span className="text-danger">{formik.errors.password}</span>
+              </div>
+              <br />
+              <div className="mb-3">
+                <input
+                  type="submit"
+                  name="submit"
+                  value="Login"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
