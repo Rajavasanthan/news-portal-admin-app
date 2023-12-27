88166434ebe2c3d765c8010c331897b77ae01508
frekerzzzz <kannamugesh135@gmail.com>
2023-12-27 11:34:31 +0530

    login seprated

--- src/App.js
+++ src/App.js
@@ -12,34 +12,38 @@ import ListAllCategory from "./components/ListAllCategory";
 import SideBar from "./components/SideBar";
 import Topbar from "./components/Topbar";
 import ViewNews from "./components/ViewNews";
+import ProtectedRouter from "./components/ProtectedRouter";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div id="wrapper">
-          <SideBar />
-          <div id="content-wrapper" className="d-flex flex-column">
-            <Topbar />
-            <div className="container-fluid">
+ 
+           
               <Routes>
                 <Route path="/" element={<Login />}></Route>
-                <Route path="/create-news" element={<CreateNews />}></Route>
-                <Route path="/edit-news" element={<EditNews />}></Route>
-                <Route path="/list-all-news" element={<ListAllNews />}></Route>
-                <Route
-                  path="/create-category"
-                  element={<CreateCategory />}
-                ></Route>
-                <Route path="/edit-category" element={<EditCategory />}></Route>
-                <Route
-                  path="/list-all-category"
-                  element={<ListAllCategory />}
-                ></Route>
-                <Route path="/view-news" element={<ViewNews />}></Route>
+                <Route element={<ProtectedRouter />}>
+                  
+                  <Route path="/create-news" element={<CreateNews />}></Route>
+                  <Route path="/edit-news" element={<EditNews />}></Route>
+                  <Route
+                    path="/list-all-news"
+                    element={<ListAllNews />}
+                  ></Route>
+                  <Route
+                    path="/create-category"
+                    element={<CreateCategory />}
+                  ></Route>
+                  <Route
+                    path="/edit-category"
+                    element={<EditCategory />}
+                  ></Route>
+                  <Route
+                    path="/list-all-category"
+                    element={<ListAllCategory />}
+                  ></Route>
+                  <Route path="/view-news" element={<ViewNews />}></Route>
+                </Route>
               </Routes>
-            </div>
-          </div>
-        </div>
       </BrowserRouter>
     </>
   );
